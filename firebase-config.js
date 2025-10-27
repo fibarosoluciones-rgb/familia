a039fd6e5b342e5a1158fed6e10e7a1ab35e3fce
fibarosoluciones-rgb <fibarosoluciones@gmail.com>
2025-10-27 19:22:42 +0100

    Configure Firebase connection

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'REEMPLAZA_CON_TU_API_KEY',
-  authDomain: 'REEMPLAZA_CON_TU_AUTH_DOMAIN',
-  projectId: 'REEMPLAZA_CON_TU_PROJECT_ID',
-  storageBucket: 'REEMPLAZA_CON_TU_STORAGE_BUCKET',
-  messagingSenderId: 'REEMPLAZA_CON_TU_SENDER_ID',
-  appId: 'REEMPLAZA_CON_TU_APP_ID'
+  apiKey: 'AIzaSyCMxZziwNGhWDtU05NhF6_MHg5AzhpKUMg',
+  authDomain: 'familia-c9e93.firebaseapp.com',
+  projectId: 'familia-c9e93',
+  storageBucket: 'familia-c9e93.firebasestorage.app',
+  messagingSenderId: '1096236210369',
+  appId: '1:1096236210369:web:21070ea5f7cb09a96edc40',
+  measurementId: 'G-JNC2C1LY0C'
 };
